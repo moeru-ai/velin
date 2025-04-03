54f8b3c088993890c5a9fe2bded6629b38568dd9
RainbowBird <git@luoling.moe>
2025-04-04 06:03:21 +0800

    feat: parse vdom

---
+++ examples/demo/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { processMarkdownToVDom } from '../../../src/markdown/parser';
+import { markdown } from './Markdown';
+
+// const result = computed(async () => await processMarkdownToVDom(markdown))
+const result = ref('')
+
+onMounted(async () => {
+  result.value = await processMarkdownToVDom(markdown)
+})
+</script>
+
+<template>
+  {{ result }}
+</template>
--- src/markdown/parser.ts
+++ src/markdown/parser.ts
@@ -1,5 +1,9 @@
-import { renderSFC } from '../sfc/parser'
+import { type RenderFunction } from 'vue'
+import { evaluateAnyModule } from '../sfc/import'
+import { compileSFCForRaw, renderSFC, resolveDataFromScriptComponent } from '../sfc/parser'
 import { convertHtmlToMarkdown, convertMarkdownToHtml, createSFC, extractScriptFromHtml } from './utils'
+import { fromDom } from 'hast-util-from-dom'
+import { renderToString } from '@vue/server-renderer'
 
 /**
  * Processes a Markdown file by converting it to HTML, extracting script content,
@@ -28,4 +32,56 @@ export async function processMarkdown(source: string): Promise<string> {
   console.log(markdownResult)
 
   return markdownResult
+}
+
+export async function processMarkdownToVDom(source: string) {
+  const html = convertMarkdownToHtml(source)
+  const { remainingHTML, scriptContent } = extractScriptFromHtml(html)
+  const sfcString = createSFC(remainingHTML, scriptContent)
+
+  const { templateResult, scriptResult } = await compileSFCForRaw(sfcString, false)
+  // console.log(templateResult.code)
+  // console.log(scriptResult.content)
+
+  // const vdom = eval(templateResult.code).render()
+  const script = await evaluateAnyModule(scriptResult.content)
+  const render = await evaluateAnyModule(templateResult.code) as RenderFunction
+
+  // console.log(script)
+  // console.log(render)
+  // const ctx = await script.setup()
+  // const vdom = render(ctx)
+
+  const ctx = await resolveDataFromScriptComponent(script)
+
+  const dom = render.call(ctx, ctx, [])
+
+  const renderedHTML = await renderToString(dom)
+  console.log(renderedHTML)
+
+  const markdownResult = await convertHtmlToMarkdown(renderedHTML)
+  console.log(markdownResult)
+
+  return markdownResult
+
+  // console.log(dom)
+
+  // const hast = fromDom(dom)
+
+  // console.log(hast)
+
+  // console.log(render.toString())
+
+  // const result = await compileSFC(sfcString)
+  // const { ssrRender, data, setup } = result
+  // console.log(ssrRender)
+  // console.log(data)
+  // console.log(setup)
+
+  // // console.log(ssrRender({}, {}, {}, {}))
+  // const vdom = createVNode({
+  //   setup,
+  //   template: ssrRender,
+  // })
+  // console.log(vdom)
 }
