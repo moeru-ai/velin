d159cf90fc7d56b12dc5c3d8934be1bfdf92f620
RainbowBird <git@luoling.moe>
2025-04-06 16:46:18 +0800

    feat: data parameters for markdown

--- examples/md.ts
+++ examples/md.ts
@@ -1,8 +1,12 @@
 import fs from 'node:fs/promises'
+import { ref } from 'vue'
 import { processMarkdown } from '../src/markdown/parser'
 
 (async () => {
   const markdownString = await fs.readFile('examples/assets/Markdown.md', 'utf-8')
 
-  await processMarkdown(markdownString)
+  const result = await processMarkdown(markdownString, {
+    language: ref('TypeScript'),
+  })
+  console.log(result)
 })()
--- src/markdown/parser.ts
+++ src/markdown/parser.ts
@@ -1,35 +1,11 @@
 import type { RenderFunction } from 'vue'
 import { renderToString } from '@vue/server-renderer'
+import defu from 'defu'
 import { evaluateAnyModule } from '../sfc/import'
-import { compileSFCForRaw, renderSFC, resolveDataFromScriptComponent } from '../sfc/parser'
+import { compileSFCForRaw, resolveDataFromScriptComponent } from '../sfc/parser'
 import { convertHtmlToMarkdown, convertMarkdownToHtml, createSFC, extractScriptFromHtml } from './utils'
 
-/**
- * Processes a Markdown file by converting it to HTML, extracting script content,
- * creating an SFC, rendering it, and converting back to Markdown.
- *
- * @param source - Markdown content or file path
- * @returns Promise that resolves when processing is complete
- */
-export async function processMarkdownSSR(source: string): Promise<string> {
-  // Convert Markdown to HTML
-  const html = convertMarkdownToHtml(source)
-
-  // Process HTML and extract script content
-  const { remainingHTML, scriptContent } = extractScriptFromHtml(html)
-
-  // Create SFC (Single File Component)
-  const sfcString = createSFC(remainingHTML, scriptContent)
-
-  // Render SFC to HTML
-  const renderedHTML = await renderSFC(sfcString)
-
-  // Convert HTML back to Markdown
-  const markdownResult = await convertHtmlToMarkdown(renderedHTML)
-  return markdownResult
-}
-
-export async function processMarkdown(source: string): Promise<string> {
+export async function processMarkdown(source: string, data?: Record<string, any>): Promise<string> {
   const html = convertMarkdownToHtml(source)
   const { remainingHTML, scriptContent } = extractScriptFromHtml(html)
   const sfcString = createSFC(remainingHTML, scriptContent)
@@ -40,7 +16,8 @@ export async function processMarkdown(source: string): Promise<string> {
   const script = await evaluateAnyModule(scriptResult.content)
   const render = await evaluateAnyModule(templateResult.code) as RenderFunction
 
-  const ctx = await resolveDataFromScriptComponent(script)
+  let ctx = await resolveDataFromScriptComponent(script)
+  ctx = defu(data || {}, ctx)
 
   const dom = render.call(ctx, ctx, [])
 
