ee9ca689e8ddcf933762f6c1a4c73d1b2a04fd94
Neko Ayaka <neko@ayaka.moe>
2025-05-05 02:21:57 +0800

    feat(apps/playground): init

---
+++ apps/playground/src/pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div flex flex-col gap-2>
+    <h2 text-xl>
+      Options
+    </h2>
+    <div w-full flex flex-row gap-2 />
+  </div>
+</template>
