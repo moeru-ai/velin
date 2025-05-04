511ee22f63a1220288e78ae06d5cfade84a80617
Neko Ayaka <neko@ayaka.moe>
2025-05-05 02:32:24 +0800

    chore(apps/playground): updated uno.config

--- apps/playground/uno.config.ts
+++ apps/playground/uno.config.ts
@@ -1,37 +1,15 @@
-import {
-  defineConfig,
-  presetAttributify,
-  presetIcons,
-  presetTypography,
-  presetWebFonts,
-  presetWind3,
-  transformerDirectives,
-  transformerVariantGroup,
-} from 'unocss'
+import { mergeConfigs } from 'unocss'
 
-export default defineConfig({
-  presets: [
-    presetWind3(),
-    presetAttributify(),
-    presetTypography(),
-    presetWebFonts({
-      fonts: {
-        mono: 'DM Mono',
-        sans: 'DM Sans',
-        serif: 'DM Serif Display',
-      },
-      timeouts: {
-        failure: 10000,
-        warning: 5000,
-      },
-    }),
-    presetIcons({
-      scale: 1.2,
-    }),
-  ],
-  safelist: 'prose prose-sm m-auto text-left'.split(' '),
-  transformers: [
-    transformerDirectives(),
-    transformerVariantGroup(),
-  ],
-})
+import { sharedUnoConfig } from '../../uno.config'
+
+export default mergeConfigs([
+  sharedUnoConfig(),
+  {
+    rules: [
+      ['transition-colors-none', {
+        'transition-property': 'color, background-color, border-color, text-color',
+        'transition-duration': '0s',
+      }],
+    ],
+  },
+])
--- uno.config.ts
+++ uno.config.ts
@@ -1,13 +1,10 @@
-import {
-  defineConfig,
-  presetAttributify,
-  presetIcons,
-  presetTypography,
-  presetWebFonts,
-  presetWind3,
-  transformerDirectives,
-  transformerVariantGroup,
-} from 'unocss'
+import type { PresetOrFactoryAwaitable } from 'unocss'
+
+import { createExternalPackageIconLoader } from '@iconify/utils/lib/loader/external-pkg'
+import { colorToString } from '@unocss/preset-mini/utils'
+import { defineConfig, mergeConfigs, presetAttributify, presetIcons, presetTypography, presetWebFonts, presetWind3, transformerDirectives, transformerVariantGroup } from 'unocss'
+import { presetScrollbar } from 'unocss-preset-scrollbar'
+import { parseColor } from 'unocss/preset-mini'
 
 function createColorSchemeConfig(hueOffset = 0) {
   return {
@@ -26,39 +23,127 @@ function createColorSchemeConfig(hueOffset = 0) {
   }
 }
 
-export default defineConfig({
-  presets: [
-    presetWind3(),
-    presetAttributify(),
-    presetTypography(),
-    presetWebFonts({
-      fonts: {
-        sans: 'DM Sans',
-        serif: 'DM Serif Display',
-        mono: 'DM Mono',
-        cute: 'Kiwi Maru',
-        cuteen: 'Sniglet',
-        jura: 'Jura',
-        gugi: 'Gugi',
-        quicksand: 'Quicksand',
+export function presetStoryMockHover(): PresetOrFactoryAwaitable {
+  return {
+    name: 'story-mock-hover',
+    variants: [
+      (matcher) => {
+        if (!matcher.includes('hover')) {
+          return matcher
+        }
+
+        return {
+          matcher,
+          selector: (s) => {
+            return `${s}, ${s.replace(/:hover$/, '')}._hover`
+          },
+        }
       },
-    }),
-    presetIcons({
-      scale: 1.2,
-    }),
-  ],
-  rules: [
-    ['write-sideways-rl', { 'writing-mode': 'sideways-rl' }],
-    ['write-sideways-lr', { 'writing-mode': 'sideways-lr' }],
-  ],
-  transformers: [
-    transformerDirectives(),
-    transformerVariantGroup(),
-  ],
-  theme: {
-    colors: {
-      primary: createColorSchemeConfig(),
-      complementary: createColorSchemeConfig(180),
+    ],
+  }
+}
+
+export function safelistAllPrimaryBackgrounds(): string[] {
+  return [
+    ...[undefined, 50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 950].map((shade) => {
+      const prefix = shade ? `bg-primary-${shade}` : `bg-primary`
+      return [
+        prefix,
+        ...[5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100].map(opacity => `${prefix}/${opacity}`),
+      ]
+    }).flat(),
+  ]
+}
+
+export function sharedUnoConfig() {
+  return defineConfig({
+    presets: [
+      presetWind3(),
+      presetAttributify(),
+      presetTypography(),
+      presetWebFonts({
+        fonts: {
+          sans: 'DM Sans',
+          serif: 'DM Serif Display',
+          mono: 'DM Mono',
+          cute: 'Kiwi Maru',
+          cuteen: 'Sniglet',
+          jura: 'Jura',
+          gugi: 'Gugi',
+          quicksand: 'Quicksand',
+          quanlai: {
+            name: 'cjkfonts AllSeto',
+            provider: 'none',
+          },
+          xiaolai: {
+            name: 'Xiaolai SC',
+            provider: 'none',
+          },
+        },
+        timeouts: {
+          warning: 5000,
+          failure: 10000,
+        },
+      }),
+      presetIcons({
+        scale: 1.2,
+        collections: {
+          ...createExternalPackageIconLoader('@proj-airi/lobe-icons'),
+        },
+      }),
+      presetScrollbar(),
+    ],
+    transformers: [
+      transformerDirectives({
+        applyVariable: ['--at-apply'],
+      }),
+      transformerVariantGroup(),
+    ],
+    safelist: [
+      ...'prose prose-sm m-auto text-left'.split(' '),
+    ],
+    // hyoban/unocss-preset-shadcn: Use shadcn ui with UnoCSS
+    // https://github.com/hyoban/unocss-preset-shadcn
+    //
+    // Thanks to
+    // https://github.com/unovue/shadcn-vue/issues/34#issuecomment-2467318118
+    // https://github.com/hyoban-template/shadcn-vue-unocss-starter
+    //
+    // By default, `.ts` and `.js` files are NOT extracted.
+    // If you want to extract them, use the following configuration.
+    // It's necessary to add the following configuration if you use shadcn-vue or shadcn-svelte.
+    content: {
+      pipeline: {
+        include: [
+          // the default
+          /\.(vue|svelte|[jt]sx|mdx?|astro|elm|php|phtml|html)($|\?)/,
+          // include js/ts files
+          '(components|src)/**/*.{js,ts,vue}',
+          '**/stage-ui/**/*.{vue,js,ts}',
+          '**/ui/**/*.{vue,js,ts}',
+        ],
+      },
     },
-  },
-})
+    rules: [
+      [/^mask-\[(.*)\]$/, ([, suffix]) => ({ '-webkit-mask-image': suffix.replace(/_/g, ' ') })],
+      [/^bg-dotted-\[(.*)\]$/, ([, color], { theme }) => {
+        const parsedColor = parseColor(color, theme)
+        // Util usage: https://github.com/unocss/unocss/blob/f57ef6ae50006a92f444738e50f3601c0d1121f2/packages-presets/preset-mini/src/_utils/utilities.ts#L186
+        return {
+          'background-image': `radial-gradient(circle at 1px 1px, ${colorToString(parsedColor?.cssColor ?? parsedColor?.color ?? color, 'var(--un-background-opacity)')} 1px, transparent 0)`,
+          '--un-background-opacity': parsedColor?.cssColor?.alpha ?? parsedColor?.alpha ?? 1,
+        }
+      }],
+    ],
+    theme: {
+      colors: {
+        primary: createColorSchemeConfig(),
+        complementary: createColorSchemeConfig(180),
+      },
+    },
+  })
+}
+
+export default mergeConfigs([
+  sharedUnoConfig(),
+])
