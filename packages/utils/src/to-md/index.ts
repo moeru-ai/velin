8653dc196b2d0b757b5fa1c91ce4cd2d164b87c0
特菈 Dustella <fdnoaivj@outlook.com>
2025-05-14 19:39:39 +0800

    fix: strip comments (#18)
    * fix: strip comments
    * fix: type infer in ts props is correct
    * chore: update packages/utils/src/to-md/index.ts
    
    ---------
    
    Co-authored-by: Neko <neko@ayaka.moe>

--- packages/utils/src/to-md/index.ts
+++ packages/utils/src/to-md/index.ts
@@ -1,11 +1,13 @@
 import rehypeParse from 'rehype-parse'
 import rehypeRemark from 'rehype-remark'
+import rehypeRemoveComments from 'rehype-remove-comments'
 import remarkStringify from 'remark-stringify'
 import { unified } from 'unified'
 
 export async function toMarkdown(html: string): Promise<string> {
   const htmlToMarkdownProcessor = unified()
     .use(rehypeParse)
+    .use(rehypeRemoveComments)
     .use(rehypeRemark)
     .use(remarkStringify, { bullet: '-' })
 
