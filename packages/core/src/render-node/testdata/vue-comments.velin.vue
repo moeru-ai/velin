593ef267d8bbdc6e87f43fce1f9accbc9d602da8
Menci <mencici@msn.com>
2026-04-21 21:21:18 +0800

    fix(utils): strip HTML comments generated by Vue

---
+++ packages/core/src/render-node/testdata/vue-comments.velin.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+
+const show = ref(true)
+const items = ref(['a', 'b', 'c'])
+</script>
+
+<template>
+  <div>
+    <h1>Title</h1>
+    <p v-if="show">visible</p>
+    <ul>
+      <li v-for="item in items" :key="item">{{ item }}</li>
+    </ul>
+  </div>
+</template>
